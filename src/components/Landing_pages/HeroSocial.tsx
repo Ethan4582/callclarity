44c3845a10d8ea99e24d9484d1396b03ae550223
ASH <singhashirwad2003@gmail.com>
2026-02-19 04:09:58 +0530

    hero section changes

---
+++ src/components/Landing_pages/HeroSocial.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const HeroSocial = () => {
+   return (
+      <div aria-label="SocialProof" className="items-start flex flex-col size-min justify-start relative gap-[4px] shrink-[0] opacity-[0.95]">
+         <div aria-label="They trust us" className="flex flex-col justify-start relative whitespace-pre shrink-[0]">
+            <p className="text-white text-[16px] tracking-[-0.64px] leading-[19.2px]" style={{ "fontFamily": "Inter, sans-serif", "textDecoration": "rgb(255, 255, 255)" }}>They trust us</p>
+         </div>
+         <div aria-label="Layout" className="items-center flex size-min justify-start relative gap-[8px] shrink-[0]">
+            <div aria-label="Stars" className="items-center flex size-min justify-start relative gap-[8px] shrink-[0]">
+               <div aria-label="Materials" className="items-center flex size-min justify-center overflow-hidden relative gap-[4px] shrink-[0]">
+                  <div className="relative w-4 h-4 shrink-[0]">
+                     <div className="contents">
+                        <div className="inline-block size-full overflow-hidden text-white fill-white" style={{ "textDecoration": "rgb(255, 255, 255)" }}>
+                           <img src="https://storage.googleapis.com/download/storage/v1/b/prd-shared-services.firebasestorage.app/o/h2m-assets%2F554722f10531deaffb0dea6df10d2a5f94684c7a.svg?generation=1771443083878893&amp;alt=media" className="inline-block size-full" />
+                        </div>
+                     </div>
+                  </div>
+                  <div className="relative w-4 h-4 shrink-[0]">
+                     <div className="contents">
+                        <div className="inline-block size-full overflow-hidden text-white fill-white" style={{ "textDecoration": "rgb(255, 255, 255)" }}>
+                           <img src="https://storage.googleapis.com/download/storage/v1/b/prd-shared-services.firebasestorage.app/o/h2m-assets%2F554722f10531deaffb0dea6df10d2a5f94684c7a.svg?generation=1771443083878893&amp;alt=media" className="inline-block size-full" />
+                        </div>
+                     </div>
+                  </div>
+                  <div className="relative w-4 h-4 shrink-[0]">
+                     <div className="contents">
+                        <div className="inline-block size-full overflow-hidden text-white fill-white" style={{ "textDecoration": "rgb(255, 255, 255)" }}>
+                           <img src="https://storage.googleapis.com/download/storage/v1/b/prd-shared-services.firebasestorage.app/o/h2m-assets%2F554722f10531deaffb0dea6df10d2a5f94684c7a.svg?generation=1771443083878893&amp;alt=media" className="inline-block size-full" />
+                        </div>
+                     </div>
+                  </div>
+                  <div className="relative w-4 h-4 shrink-[0]">
+                     <div className="contents">
+                        <div className="inline-block size-full overflow-hidden text-white fill-white" style={{ "textDecoration": "rgb(255, 255, 255)" }}>
+                           <img src="https://storage.googleapis.com/download/storage/v1/b/prd-shared-services.firebasestorage.app/o/h2m-assets%2F554722f10531deaffb0dea6df10d2a5f94684c7a.svg?generation=1771443083878893&amp;alt=media" className="inline-block size-full" />
+                        </div>
+                     </div>
+                  </div>
+                  <div aria-label="Material V2" className="overflow-hidden relative w-[10px] h-4 shrink-[0]">
+                     <div className="absolute left-0 top-0 right-[-6px] bottom-0">
+                        <div className="contents">
+                           <div className="inline-block size-full overflow-hidden text-white fill-white" style={{ "textDecoration": "rgb(255, 255, 255)" }}>
+                              <img src="https://storage.googleapis.com/download/storage/v1/b/prd-shared-services.firebasestorage.app/o/h2m-assets%2F554722f10531deaffb0dea6df10d2a5f94684c7a.svg?generation=1771443083878893&amp;alt=media" className="inline-block size-full" />
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div aria-label="4,9" className="flex flex-col justify-center relative whitespace-pre shrink-[0]">
+                  <p className="font-medium text-white text-[16px] leading-[24px]" style={{ "fontFamily": "\"DM Sans\", \"DM Sans Placeholder\", sans-serif", "textDecoration": "rgb(255, 255, 255)" }}>4,8</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default HeroSocial
